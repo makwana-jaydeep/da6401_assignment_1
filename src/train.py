05162d1625196c1dbbe42d99d34a24a0dfac6258
makwana-jaydeep <2101030400151@silveroakuni.ac.in>
2026-03-04 20:27:05 +0530

    changed inference file

--- src/train.py
+++ src/train.py
@@ -26,7 +26,7 @@ def parse_arguments():
     parser.add_argument("-l", "--loss", type=str, default="cross_entropy",
                         choices=["cross_entropy", "mse"])
     parser.add_argument("-o", "--optimizer", type=str, default="rmsprop",
-                        choices=["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"])
+                        choices=["sgd", "momentum", "nag", "rmsprop"])
     parser.add_argument("-lr", "--learning_rate", type=float, default=1e-3)
     parser.add_argument("-wd", "--weight_decay", type=float, default=0.0)
     parser.add_argument("-nhl", "--num_layers", type=int, default=4)
